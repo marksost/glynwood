95d1f99646d122d2e86d036e51cdb0c9e3160330
Mark Sost <sostmark@gmail.com>
2024-10-31 11:31:31 -0400

    Adams and EB support

---
+++ cut-gen-eb.js
@@ -0,0 +1,74 @@
+const format = require('date-format');
+const fs = require('fs');
+const { parse } = require('csv');
+const { PDFDocument } = require('pdf-lib');
+const slugify = require('slugify');
+
+const PDFName = 'Eagle Bridge Label - Template.pdf';
+const CSVName = 'eagle-bridge-cuts.csv';
+let fileCount = 0;
+
+const readCSV = async () => {
+  console.log(`Reading CSV: ${CSVName}`);
+
+  const records = [];
+  const parser = fs.createReadStream(`./${CSVName}`).pipe(parse({}));
+
+  for await (const record of parser) {
+    records.push(record);
+  }
+
+  return records;
+};
+
+const getPDFTemplate = async () => {
+  console.log(`Reading PDF: ${PDFName}`);
+
+  const file = fs.readFileSync(`./${PDFName}`, null);
+  return await PDFDocument.load(file);
+};
+
+const savePDF = async (pdfDoc, path) => {
+  console.log(`Saving PDF file: ${path}`);
+
+  const filledFormBytes = await pdfDoc.save();
+  fs.writeFileSync(path, filledFormBytes);
+
+  fileCount++;
+
+  console.log('PDF saved successfully!');
+};
+
+const setDynamicFields = (form, cut) => {
+  console.log(`Setting dynamic fields for cut: ${cut[0]}`);
+
+  // Dynamic fields
+  const productName = form.getTextField('ProductName');
+  const weight = form.getTextField('Weight');
+
+  productName.setText(cut[0].toUpperCase());
+  weight.setText(cut[1] + 'lbs');
+};
+
+async function main() {
+  console.log('Starting PDF generation...');
+
+  const cuts = await readCSV();
+  const pdfDoc = await getPDFTemplate();
+  const form = pdfDoc.getForm();
+
+  console.log(`Setting dynamic fields for ${cuts.length - 1} cuts...`);
+
+  for (let i = 1; i < cuts.length; i++ ) {
+    const cut = cuts[i];
+    const slug = slugify(cut[0] + ' ' + cut[1]);
+
+    setDynamicFields(form, cut);
+
+    await savePDF(pdfDoc, `./eb-outputs/${PDFName.replace('Template', slug)}`);
+  }
+
+  console.log(`Processed ${fileCount} PDFs!`);
+}
+
+main().catch((err) => console.error('Error:', err));
